8a691bbee2cee4999eace80adfee34e41e345a8c
omaralabtran <albatranomar943@gmail.com>
2020-08-25 21:35:20 +0300

    add "invite" command

---
+++ src/commands/public/invite.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const { Command } = require('discord-akairo');
+
+module.exports = class extends Command {
+  constructor() {
+    super('invite', {
+      aliases: ['invite', 'inv', 'addme'],
+      cooldown: 10000,
+      ratelimit: 2
+    });
+  }
+
+  exec(message) {
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Click Me To Add [Quran Bot]`)
+      .setURL("https://discordapp.com/api/oauth2/authorize?client_id=692060368780001300&permissions=104132416&scope=bot")
+      .setDescription("**مرحباً انا بوت القرآن الكريم ان كنت تريديني ارجو منك الضغط على اضافة البوت في حالة لديك مشكلة في البوت توجه لدعم الفني **" + `\n` + `**Support [دعم الفني](https://discord.gg/wxhbgn)**`)
+      .setThumbnail(message.client.user.displayAvatarURL())
+      .setColor('#3374FF')
+    message.author.send(embed).then(() => {
+      message.react("✅");
+    }).catch(() => {
+      message.react("❌");
+    });
+  }
+}
