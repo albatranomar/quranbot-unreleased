5683d11d7f4307f42c425d47531d8e4552cbfd01
omaralabtran <albatranomar943@gmail.com>
2020-08-26 20:17:56 +0300

    add 'now playing' command

---
+++ src/commands/quran/nowplaying.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const { Command } = require('discord-akairo');
+const surahs = require("../../quran-json/surahs.pretty.json");
+const readers = require("../../quran-json/readers.json");
+
+module.exports = class extends Command {
+  constructor() {
+    super('nowplaying', {
+      aliases: ['nowplaying', 'now-playing', 'الان', 'حالي', 'np'],
+      category: 'quran',
+      cooldown: 10000,
+      ratelimit: 2,
+      channel: "guild"
+    });
+  }
+  /**
+  * 
+  * @param {Discord.Message} message 
+  * @param {*} args 
+  */
+  exec(message, args) {
+    let serverQueue = this.client.guilds_settings.get(message.guild.id, 'quran_queue');
+		if (!serverQueue) return `**لا يوجد شيء تستمع اليه حاليا. **`;
+		return `** انت تستمع حاليا الى : \n \`${serverQueue.songs[0].title}\`**`;
+  }
+}
