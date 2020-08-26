f10d4f9abc540ebe182e4be1cbfedf6d43b63f63
omaralabtran <albatranomar943@gmail.com>
2020-08-26 14:34:34 +0300

    fixed

--- src/commands/developers/bot-info.js
+++ src/commands/developers/bot-info.js
@@ -4,7 +4,7 @@ const { Command } = require('discord-akairo');
 module.exports = class extends Command {
   constructor() {
     super('bot-info', {
-      aliases: ['bot-info', 'botinfo', 'bot.info'],
+      aliases: ['bot-info', 'bot.info'],
       cooldown: 10000,
       ratelimit: 2,
       ownerOnly: true
