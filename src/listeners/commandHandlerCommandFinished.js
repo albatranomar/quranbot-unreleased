1b3ce1159bf7807bc67e4bb7fb3ddbe497b01d35
omaralabtran <albatranomar943@gmail.com>
2020-08-26 12:47:50 +0300

    make 'verse' command && add 'commandFinished' event

---
+++ src/listeners/commandHandlerCommandFinished.js
@@ -0,0 +1,22 @@
+const { Listener } = require("discord-akairo");
+const { Message } = require("discord.js");
+
+module.exports = class extends Listener {
+  constructor() {
+    super('commandFinished', {
+      emitter: 'commandHandler',
+      event: 'commandFinished',
+      category: 'commandHandler'
+    });
+  }
+  /**
+   * 
+   * @param {Message} message 
+   * @param {*} command 
+   * @param {*} args 
+   * @param {*} returnValue 
+   */
+  exec(message, command, args, returnValue) {
+    message.util.reply(returnValue);
+  }
+}
