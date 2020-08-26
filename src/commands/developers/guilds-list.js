305c6f7d5c1161d55af75bd1d6214b57fe38904b
omaralabtran <albatranomar943@gmail.com>
2020-08-26 14:07:13 +0300

    add 'guilds-list' command

---
+++ src/commands/developers/guilds-list.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const { Command } = require('discord-akairo');
+
+module.exports = class extends Command {
+  constructor() {
+    super('guilds-list', {
+      aliases: ['guilds-list', 'servers-list'],
+      cooldown: 10000,
+      ratelimit: 2,
+      ownerOnly: true
+    });
+  }
+  /**
+   * 
+   * @param {Discord.Message} message 
+   * @param {*} args 
+   */
+  exec(message, args) {
+    let txtServers = `How many servers? [${message.client.guilds.cache.size}]\n\n\n` + message.client.guilds.cache.map((guild, i) => `${i+1}- [${guild.name}]`).join("\n");
+    let serversAttach = new Discord.MessageAttachment(Buffer.from(txtServers, 'utf8'), "servers.txt");
+    message.channel.send(`${message.author}, هذا ملف نصي يحتي على أسماء جميع سيرفرات البوت.`, {
+      files: [ serversAttach ]
+    });
+  }
+}
