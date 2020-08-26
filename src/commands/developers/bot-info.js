6156a2823b7661c0ba8ec8f51d2f624c0289d385
omaralabtran <albatranomar943@gmail.com>
2020-08-26 14:28:36 +0300

    add 'bot-info' command

---
+++ src/commands/developers/bot-info.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const { Command } = require('discord-akairo');
+
+module.exports = class extends Command {
+  constructor() {
+    super('bot-info', {
+      aliases: ['bot-info', 'botinfo', 'bot.info'],
+      cooldown: 10000,
+      ratelimit: 2,
+      ownerOnly: true
+    });
+  }
+  /**
+   * 
+   * @param {Discord.Message} message 
+   * @param {*} args 
+   */
+  exec(message, args) {
+    message.channel.send("", {
+      embed: new Discord.MessageEmbed()
+      .setAuthor(message.author.username, message.author.displayAvatarURL())
+      .setColor("RANDOM")
+      .setThumbnail(message.client.user.displayAvatarURL())
+      .setFooter(`إذا كنت تريد قائمة بأسماء السيرفرات يمكنك كتابة الأمر التالي. [${message.client.config.prefix}servers].`, message.client.user.displayAvatarURL())
+      .addField(`**Guilds:**`, `**\`${message.client.guilds.cache.size}\` Servers.**`)
+      .addField(`**Users:**`, `**\`${message.client.users.cache.size}\` Users.**`)
+    });
+  }
+}
