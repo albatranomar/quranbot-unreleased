65993aacb11639a1bf57ab345e422c75207a5726
Omar Al Batran <omarb@eummena.org>
2020-08-27 13:32:02 +0300

    makes updates

--- src/commands/public/help.js
+++ src/commands/public/help.js
@@ -1,45 +1,67 @@
-const Discord = require("discord.js");
-const { Command } = require('discord-akairo');
+const { Message, MessageEmbed } = require("discord.js");
+const { Command } = require("discord-akairo");
+const { Menu } = require('discord.js-menu');
 
 module.exports = class extends Command {
-  constructor() {
-    super('help', {
-      aliases: ['help', 'hlp', 'h', 'commands', 'cmds'],
-      category: 'public',
-      cooldown: 10000,
-      ratelimit: 2
-    });
-  }
+    constructor() {
+            super("help", {
+                aliases: ["help", "hlp", "h", "commands", "cmds", "مساعدة", "المساعدة"],
+                category: "public",
+                cooldown: 10000,
+                ratelimit: 2,
+            });
+        }
+        /**
+         *
+         * @param {Message} message
+         */
+    exec(message) {
+        let prefix = this.client.config.prefix;
+        let reactions = {
+            '⏪': "first",
+            '◀': "previous",
+            '⏹': "stop",
+            '▶': "next",
+            '⏩': "last"
+        }
+        let HelpEmbedDefult = new MessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+            .setFooter(this.client.user.tag, this.client.user.displayAvatarURL())
+            .setTimestamp()
+            .setColor("RANDOM").toJSON();
 
-  exec(message) {
-    let embed = new Discord.MessageEmbed()
-      .setAuthor(message.author.tag, message.author.avatarURL)
-      .setColor("RANDOM")
-      .setThumbnail("https://cdn.discordapp.com/avatars/692060368780001300/94e577ebe3a8970c72ccbbca968309f8.webp")
-      .addField('**`أوامر الإدارة`**', `**⇩**`)
-      .addField('**-تكرار**', `**تكرار المقطع المشغل**`)
-      .addField('**-تحديد**', `**تعين روم الاوامر (أختياري)**`)
-      .addField('**`أوامر الإستماع`**', `**⇩**`)
-      .addField('**-القران**', `**امر تشغيل,-القران كامل لتشغيل سورة معينة -البقرة 100 يمكنك اسخدام سور اخرى**`)
-      .addField('**-حالي**', `**عرض ما تستمع اليه الان**`)
-      .addField('**-تعليق**', `**ايقاف سماع القران الكريم مؤقتا**`)
-      .addField('**-الانتظار**', `**امر لعرض قائمة الانتظار.**`)
-      .addField('**-استئنف**', `**بدء الإستماع الى القران الكريم من جديد**`)
-      .addField('**-تخطي**', `**امر التخطي.عمل تخطي للمقطع الذي تستمع اليه والانتقال الى المقطع التالي في قائمة الانتظار**`)
-      .addField('**-توقف**', `**مسح قائمة الإنتظار**`)
-      .addField('**-صوت**', `**تعديل مستوى صوت القران الكريم [1-100]**`)
-      .addField('**`أوامر عامة`**', `**⇩**`)
-      .addField('**-help**', `**امر المساعدة**`)
-      .addField('**-inv**', `**اضافة البوت الى سيرفرك**`)
-      .addField('**-سرعة**', `**معرفة سرعة البوت**`)
-      .addField('**-القراء**', `**عرض القراء الذي يمكن الأستماع إليهم**`)
-      .addField('**-اية**', `**أمر اية الذي يرسل لك صورة لأي اية من القران الكريم-اية سورة رقم**`)
-      .addField('**`أخرى`**', `**Website [View](http://quranbot.ml/)|Link Bot [Add](https://discordapp.com/api/oauth2/authorize?client_id=692060368780001300&permissions=8&scope=bot)|Support [join](https://discord.gg/3rZjSyS)
-    **`)
-    message.author.send(embed).then(() => {
-      message.react("✅");
-    }).catch(() => {
-      message.react("❌");
-    });
-  }
-}
+        let helpPages = [{
+                name: "quran",
+                content: new MessageEmbed(HelpEmbedDefult).setTitle(`الأوامر القران الكريم`).addFields(this.client.commandHandler.modules.filter(c => c.category == "quran").map((c, id) => { return { name: id, value: c.aliases.join('|') } })),
+                reactions
+            },
+            {
+                name: "moderation",
+                content: new MessageEmbed(HelpEmbedDefult).setTitle(`الأوامر الإدارية`).addFields(this.client.commandHandler.modules.filter(c => c.category == "moderation").map((c, id) => { return { name: id, value: c.aliases.join('|') } })),
+                reactions
+            },
+            {
+                name: "public",
+                content: new MessageEmbed(HelpEmbedDefult).setTitle(`الأوامر العامة`).addFields(this.client.commandHandler.modules.filter(c => c.category == "public").map((c, id) => { return { name: id, value: c.aliases.join('|') } })),
+                reactions
+            }
+        ]
+
+        if (this.client.isOwner(message.author.id)) {
+            helpPages.push({
+                name: "developers",
+                content: new MessageEmbed(HelpEmbedDefult).setTitle(`الأوامر المطورين`).addFields(this.client.commandHandler.modules.filter(c => c.category == "developers").map((c, id) => { return { name: id, value: c.aliases.join('|') } })),
+                reactions
+            });
+        }
+
+        message.author.send(`${message.author}`).then((dmMessage) => {
+                message.react("✅");
+                new Menu(dmMessage.channel, message.author.id, helpPages);
+            })
+            .catch((e) => {
+                console.log(`${e}`);
+                message.react("❌");
+            });
+    }
+};
