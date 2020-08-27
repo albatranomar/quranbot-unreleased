8309c33c238feb445a86eb824d000809d4526e66
Omar Al Batran <omarb@eummena.org>
2020-08-27 13:32:18 +0300

    makes updates,

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const QuranBot = new _QuranBot(config);
 
 QuranBot.start(config.token);
 
-
+process.on("unhandledRejection", (r, p) => {
+    if (`${r}` == "DiscordAPIError: Cannot execute action on a DM channel") return console.warn(`unhandledRejection: ${r} [HELP EMBED CANOT CLEAR REACION IN DM]`);
+    console.warn(r);
+})
