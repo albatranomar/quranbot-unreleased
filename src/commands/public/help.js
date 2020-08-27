8f9488e480ca750e342aa6d1e52c323cca705ce2
omaralabtran <albatranomar943@gmail.com>
2020-08-27 20:52:22 +0300

    add 'Thumbnails' for help embeds

--- src/commands/public/help.js
+++ src/commands/public/help.js
@@ -33,17 +33,26 @@ module.exports = class extends Command {
 
     let helpPages = [{
       name: "quran",
-      content: new MessageEmbed(HelpEmbedDefult).setTitle(`الأوامر القران الكريم`).addFields(this.client.commandHandler.modules.filter(c => c.category == "quran").map((c, id) => { return { name: `${prefix}${id}`, value: c.aliases.join('|') } })),
+      content: new MessageEmbed(HelpEmbedDefult)
+        .setTitle(`الأوامر القران الكريم`)
+        .setThumbnail(`https://media.discordapp.net/attachments/690314090500981189/748519538924191784/d0fe42613a173ec1.png?width=425&height=425`)
+        .addFields(this.client.commandHandler.modules.filter(c => c.category == "quran").sort((c, i) => (i == "play") ? 1 : -1).map((c, id) => { return { name: `${prefix}${id}`, value: c.aliases.join('|') } })),
       reactions
     },
     {
       name: "moderation",
-      content: new MessageEmbed(HelpEmbedDefult).setTitle(`الأوامر الإدارية`).addFields(this.client.commandHandler.modules.filter(c => c.category == "moderation").map((c, id) => { return { name: `${prefix}${id}`, value: c.aliases.join('|') } })),
+      content: new MessageEmbed(HelpEmbedDefult)
+        .setTitle(`الأوامر الإدارية`)
+        .setThumbnail(`https://media.discordapp.net/attachments/690314090500981189/748519556523229266/1292c1d55de9c96a.png?width=430&height=430`)
+        .addFields(this.client.commandHandler.modules.filter(c => c.category == "moderation").map((c, id) => { return { name: `${prefix}${id}`, value: c.aliases.join('|') } })),
       reactions
     },
     {
       name: "public",
-      content: new MessageEmbed(HelpEmbedDefult).setTitle(`الأوامر العامة`).setDescription(`**Website [View](http://quranbot.ml/)|Link Bot [Add](https://discordapp.com/api/oauth2/authorize?client_id=692060368780001300&permissions=8&scope=bot)|Support [join](https://discord.gg/3rZjSyS)**`).addFields(this.client.commandHandler.modules.filter(c => c.category == "public").map((c, id) => { return { name: `${prefix}${id}`, value: c.aliases.join('|') } })),
+      content: new MessageEmbed(HelpEmbedDefult)
+        .setTitle(`الأوامر العامة`)
+        .setThumbnail(`https://media.discordapp.net/attachments/690314090500981189/748519579680112699/ce9869247e96061e.png?width=430&height=430`)
+        .setDescription(`**Website [View](http://quranbot.ml/)|Link Bot [Add](https://discordapp.com/api/oauth2/authorize?client_id=692060368780001300&permissions=8&scope=bot)|Support [join](https://discord.gg/3rZjSyS)**`).addFields(this.client.commandHandler.modules.filter(c => c.category == "public").map((c, id) => { return { name: `${prefix}${id}`, value: c.aliases.join('|') } })),
       reactions
     }
     ]
