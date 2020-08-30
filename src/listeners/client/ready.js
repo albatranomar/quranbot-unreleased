7a0f37753942de1405567419173150b82e453b6c
Omar Al Batran <omarb@eummena.org>
2020-08-30 13:27:14 +0300

    add more 15 secounds to backup

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -2,10 +2,10 @@ const { Listener } = require("discord-akairo");
 
 class MessageListener extends Listener {
   constructor() {
-    super('ready', {
-      emitter: 'client',
-      event: 'ready',
-      category: 'client'
+    super("ready", {
+      emitter: "client",
+      event: "ready",
+      category: "client",
     });
   }
 
@@ -18,102 +18,120 @@ class MessageListener extends Listener {
       {
         name: "{prefix}inv -> Add Bot to your server.",
         status: "online",
-        type: "PLAYING"
-      }, {
+        type: "PLAYING",
+      },
+      {
         name: "{prefix}help -> Support server & our website.",
         status: "dnd",
-        type: "WATCHING"
-      }, {
+        type: "WATCHING",
+      },
+      {
         name: "{prefix}help -> لسيرفر الدعم و الموقع الخاص بنا.",
         status: "dnd",
-        type: "WATCHING"
-      }, {
+        type: "WATCHING",
+      },
+      {
         name: "{prefix}inv -> لإضافة البوت الى سيرفر.",
         status: "idle",
-        type: "PLAYING"
-      }, {
+        type: "PLAYING",
+      },
+      {
         name: "بوت القران الكريم 💕",
         status: "dnd",
-        type: "PLAYING"
-      }, {
+        type: "PLAYING",
+      },
+      {
         name: "سرعة , دعم فني, جودة 🛡️",
         status: "dnd",
-        type: "WATCHING"
-      }, {
+        type: "WATCHING",
+      },
+      {
         name: "17 من القراء الذين تخشع لهم الآذان",
         status: "idle",
-        type: "LISTENING"
-      }
+        type: "LISTENING",
+      },
     ];
     this.costumActivitys(activitys);
     // Quran Reconnect With all Lost connictions.
-    let connections = this.client.guilds_settings.items.filter((d, k) => 'quran_queue' in d).map((d, k) => [k, d["quran_queue"]]);
+    let connections = this.client.guilds_settings.items
+      .filter((d, k) => "quran_queue" in d)
+      .map((d, k) => [k, d["quran_queue"]]);
     for (const lostedConnection of connections) {
-      let cc = this.client.channels.cache.get(lostedConnection[1].voiceChannelID);
+      let cc = this.client.channels.cache.get(
+        lostedConnection[1].voiceChannelID
+      );
       if (cc) {
         let conniction = await cc.join();
         this.client.quran_connections.set(lostedConnection[0], conniction);
-        this.playQuranThatLost(lostedConnection[1].songs[0], lostedConnection[0]);
+        this.playQuranThatLost(
+          lostedConnection[1].songs[0],
+          lostedConnection[0]
+        );
       }
-      await this.sleeep(5000);
+      await this.sleeep(20000);
     }
   }
 
   costumActivitys(acts) {
     let pick = require("pick-random");
-    let time = Math.floor(((Math.random() * (90 - 60 + 1)) + 60) * 1000);
+    let time = Math.floor((Math.random() * (90 - 60 + 1) + 60) * 1000);
     let act = pick(acts, { count: 1 })[0];
-    this.client.user.setPresence({
-      activity: {
-        name: act.name.replace(/{prefix}/g, this.client.config.prefix),
-        type: act.type
-      },
-      status: act.status
-    }).then((presence) => {
-      setTimeout(() => {
-        this.costumActivitys(acts);
-      }, time);
-    }).catch(console.log);
+    this.client.user
+      .setPresence({
+        activity: {
+          name: act.name.replace(/{prefix}/g, this.client.config.prefix),
+          type: act.type,
+        },
+        status: act.status,
+      })
+      .then((presence) => {
+        setTimeout(() => {
+          this.costumActivitys(acts);
+        }, time);
+      })
+      .catch(console.log);
   }
 
   async playQuranThatLost(song, guildId) {
-    const queue = this.client.guilds_settings.get(guildId, 'quran_queue');
+    const queue = this.client.guilds_settings.get(guildId, "quran_queue");
     if (!song) {
-      this.client.guilds_settings.delete(guildId, 'quran_queue');
+      this.client.guilds_settings.delete(guildId, "quran_queue");
       this.client.quran_connections.get(guildId).disconnect();
       return;
     }
 
-    const dispatcher = this.client.quran_connections.get(guildId).play(song.url.replace("{number}", song.nowVerse))
-      .on('finish', () => {
-        if (this.client.guilds_settings.get(guildId, 'quran_queue')) {
+    const dispatcher = this.client.quran_connections
+      .get(guildId)
+      .play(song.url.replace("{number}", song.nowVerse))
+      .on("finish", () => {
+        if (this.client.guilds_settings.get(guildId, "quran_queue")) {
           if (song.nowVerse == song.endVerse) {
             if (queue.repeat) {
               queue.songs[0].nowVerse = queue.songs[0].startVerse;
-              this.client.guilds_settings.set(guildId, 'quran_queue', queue);
+              this.client.guilds_settings.set(guildId, "quran_queue", queue);
               this.playQuranThatLost(queue.songs[0], guildId);
             } else {
               queue.songs.shift();
-              this.client.guilds_settings.set(guildId, 'quran_queue', queue);
+              this.client.guilds_settings.set(guildId, "quran_queue", queue);
               this.playQuranThatLost(queue.songs[0], guildId);
             }
           } else {
             queue.songs[0].nowVerse++;
-            this.client.guilds_settings.set(guildId, 'quran_queue', queue);
+            this.client.guilds_settings.set(guildId, "quran_queue", queue);
             this.playQuranThatLost(queue.songs[0], guildId);
           }
         }
       })
-      .on('error', error => console.error(error));
+      .on("error", (error) => console.error(error));
     dispatcher.setVolumeLogarithmic(queue.volume / 50);
-  };
+  }
 
   async sleeep(time) {
     return new Promise((res, rej) => {
       setTimeout(() => {
         res(1);
       }, time);
-    })
+    });
   }
 }
 
