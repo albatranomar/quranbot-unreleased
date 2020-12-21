ce043afce09cf5da18de4d11deae8af3a25af2f2
omaralabtran <albatranomar943@gmail.com>
2020-12-21 16:32:54 +0200

    clean a misss

--- src/sharding.js
+++
@@ -1,5 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./index.js', { totalShards: 5 });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
